8ec58b6ffd176774280e050c66b5b958dfd006e8
Happier <zhf5256@qq.com>
2025-04-06 21:46:46 +0800

    feat: 完成登录注册功能

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+  @MinLength(6)
+  @MaxLength(16)
+  public readonly username: string;
+
+  @MinLength(8)
+  @MaxLength(20)
+  public readonly password: string;
+}
