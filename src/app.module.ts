de5e8e232439489d215ae8d1c50eee7cf6e0f137
赵鸿飞 <zhaohongfei@vasoyn.com>
2025-04-09 10:54:00 +0800

    feat: 增加增删改查模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { MeetingModule } from './meeting/meeting.module';
+import { RoomModule } from './room/room.module';
+import { DeviceModule } from './device/device.module';
 
 @Module({
   imports: [
@@ -10,6 +14,10 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
+    MeetingModule,
+    RoomModule,
+    DeviceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
