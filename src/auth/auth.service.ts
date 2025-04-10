7387b93094ab77cb3d4f517bc266d222b9ccf217
Happier <zhf5256@qq.com>
2025-04-10 22:29:10 +0800

    feat: 增加用户信息

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -43,6 +43,11 @@ export class AuthService {
     const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      user: {
+        name: user.username,
+        id: user.id,
+        role: user.role,
+      },
     };
   }
 }
